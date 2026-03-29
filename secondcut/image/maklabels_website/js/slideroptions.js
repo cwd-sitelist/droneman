3a77571141e70ef00f3462b160267be8c36f3ae9
Tamil <252454095+cwd-sitelist@users.noreply.github.com>
2026-03-29 15:43:35 +0530

    secondcut

---
+++ secondcut/image/maklabels_website/js/slideroptions.js
@@ -0,0 +1,565 @@
+ 
+ //  Slider itacumens
+ 
+window.theme = {};
+
+
+(function(theme, $) {
+
+	theme = theme || {};
+
+	var instanceName = '__revolution';
+
+	var PluginRevolutionSlider = function($el, opts) {
+		return this.initialize($el, opts);
+	};
+
+	PluginRevolutionSlider.defaults = {
+		dottedOverlay: 'none',
+		delay: 9000,
+		startwidth: 1170,
+		startheight: 500,
+		hideThumbs: 200,
+ 
+		thumbWidth: 100,
+		thumbHeight: 50,
+		thumbAmount: 3,
+
+		navigationType: 'none',
+		navigationArrows: 'solo',
+		navigationStyle: 'round',
+
+		touchenabled: 'on',
+		onHoverStop: 'on',
+
+		swipe_velocity: 0.7,
+		swipe_min_touches: 1,
+		swipe_max_touches: 1,
+		drag_block_vertical: false,
+
+		keyboardNavigation: 'on',
+
+		navigationHAlign: 'center',
+		navigationVAlign: 'bottom',
+		navigationHOffset: 0,
+		navigationVOffset: 20,
+
+		soloArrowLeftHalign: 'left',
+		soloArrowLeftValign: 'center',
+		soloArrowLeftHOffset: 20,
+		soloArrowLeftVOffset: 0,
+
+		soloArrowRightHalign: 'right',
+		soloArrowRightValign: 'center',
+		soloArrowRightHOffset: 20,
+		soloArrowRightVOffset: 0,
+
+		shadow: 0,
+		fullWidth: 'on',
+		fullScreen: 'off',
+
+		spinner: 'spinner0',
+
+		stopLoop: 'off',
+		stopAfterLoops: -1,
+		stopAtSlide: -1,
+
+		shuffle: 'off',
+
+		autoHeight: 'off',
+		forceFullWidth: 'off',
+
+		hideThumbsOnMobile: 'off',
+		hideNavDelayOnMobile: 1500,
+		hideBulletsOnMobile: 'off',
+		hideArrowsOnMobile: 'off',
+		hideThumbsUnderResolution: 0,
+
+		hideSliderAtLimit: 0,
+		hideCaptionAtLimit: 0,
+		hideAllCaptionAtLilmit: 0,
+		startWithSlide: 0
+	};
+
+	PluginRevolutionSlider.prototype = {
+		initialize: function($el, opts) {
+			if ($el.data(instanceName)) {
+				return this;
+			}
+
+			this.$el = $el;
+
+			this
+				.setData()
+				.setOptions(opts)
+				.build();
+
+			return this;
+		},
+
+		setData: function() {
+			this.$el.data(instanceName, this);
+
+			return this;
+		},
+
+		setOptions: function(opts) {
+			this.options = $.extend(true, {}, PluginRevolutionSlider.defaults, opts, {
+				wrapper: this.$el
+			});
+
+			return this;
+		},
+
+		build: function() {
+			if (!($.isFunction($.fn.revolution))) {
+				return this;
+			}
+
+			this.options.wrapper.revolution(this.options);
+
+			return this;
+		}
+	};
+
+	// expose to scope
+	$.extend(theme, {
+		PluginRevolutionSlider: PluginRevolutionSlider
+	});
+
+	// jquery plugin
+	$.fn.themePluginRevolutionSlider = function(opts) {
+		return this.map(function() {
+			var $this = $(this);
+
+			if ($this.data(instanceName)) {
+				return $this.data(instanceName);
+			} else {
+				return new PluginRevolutionSlider($this, opts);
+			}
+
+		});
+	}
+
+}).apply(this, [window.theme, jQuery]);
+
+
+// Revolution Slider
+(function($) {
+
+	'use strict';
+
+	if ($.isFunction($.fn['themePluginRevolutionSlider'])) {
+
+		$(function() {
+			$('[data-plugin-revolution-slider]:not(.manual), .slider-container .slider:not(.manual)').each(function() {
+				var $this = $(this),
+					opts;
+
+				var pluginOptions = $this.data('plugin-options');
+				if (pluginOptions)
+					opts = pluginOptions;
+
+				$this.themePluginRevolutionSlider(opts);
+			});
+		});
+
+	}
+
+}).apply(this, [jQuery]);
+
+// Sort
+(function($) {
+
+	'use strict';
+
+	if ($.isFunction($.fn['themePluginSort'])) {
+
+		$(function() {
+			$('[data-plugin-sort]:not(.manual), .sort-source:not(.manual)').each(function() {
+				var $this = $(this),
+					opts;
+
+				var pluginOptions = $this.data('plugin-options');
+				if (pluginOptions)
+					opts = pluginOptions;
+
+				$this.themePluginSort(opts);
+			});
+		});
+
+	}
+
+}).apply(this, [jQuery]);
+
+// Match Height
+(function($) {
+
+	'use strict';
+
+	if ($.isFunction($.fn['themePluginMatchHeight'])) {
+
+		$(function() {
+			$('[data-plugin-match-height]:not(.manual)').each(function() {
+				var $this = $(this),
+					opts;
+
+				var pluginOptions = $this.data('plugin-options');
+				if (pluginOptions)
+					opts = pluginOptions;
+
+				$this.themePluginMatchHeight(opts);
+			});
+		});
+
+	}
+
+}).apply(this, [jQuery]);
+
+// Toggle
+(function($) {
+
+	'use strict';
+
+	if ($.isFunction($.fn['themePluginToggle'])) {
+
+		$(function() {
+			$('[data-plugin-toggle]:not(.manual)').each(function() {
+				var $this = $(this),
+					opts;
+
+				var pluginOptions = $this.data('plugin-options');
+				if (pluginOptions)
+					opts = pluginOptions;
+
+				$this.themePluginToggle(opts);
+			});
+		});
+
+	}
+
+}).apply(this, [jQuery]);
+
+// Tweets
+(function($) {
+
+	'use strict';
+
+	if ($.isFunction($.fn['themePluginTweets'])) {
+
+		$(function() {
+			$('[data-plugin-tweets]:not(.manual)').each(function() {
+				var $this = $(this),
+					opts;
+
+				var pluginOptions = $this.data('plugin-options');
+				if (pluginOptions)
+					opts = pluginOptions;
+
+				$this.themePluginTweets(opts);
+			});
+		});
+
+	}
+
+}).apply(this, [jQuery]);
+
+// Video Background
+(function($) {
+
+	'use strict';
+
+	if ($.isFunction($.fn['themePluginVideoBackground'])) {
+
+		$(function() {
+			$('[data-plugin-video-background]:not(.manual)').each(function() {
+				var $this = $(this),
+					opts;
+
+				var pluginOptions = $this.data('plugin-options');
+				if (pluginOptions)
+					opts = pluginOptions;
+
+				$this.themePluginVideoBackground(opts);
+			});
+		});
+
+	}
+
+}).apply(this, [jQuery]);
+
+// Animate
+(function(theme, $) {
+
+	theme = theme || {};
+
+	var instanceName = '__animate';
+
+	var PluginAnimate = function($el, opts) {
+		return this.initialize($el, opts);
+	};
+
+	PluginAnimate.defaults = {
+		accX: 0,
+		accY: -150,
+		delay: 1
+	};
+
+	PluginAnimate.prototype = {
+		initialize: function($el, opts) {
+			if ($el.data(instanceName)) {
+				return this;
+			}
+
+			this.$el = $el;
+
+			this
+				.setData()
+				.setOptions(opts)
+				.build();
+
+			return this;
+		},
+
+		setData: function() {
+			this.$el.data(instanceName, this);
+
+			return this;
+		},
+
+		setOptions: function(opts) {
+			this.options = $.extend(true, {}, PluginAnimate.defaults, opts, {
+				wrapper: this.$el
+			});
+
+			return this;
+		},
+
+		build: function() {
+			var self = this,
+				$el = this.options.wrapper,
+				delay = 0;
+
+			$el.addClass('appear-animation');
+
+			if (!$('html').hasClass('no-csstransitions') && $(window).width() > 767) {
+
+				$el.appear(function() {
+
+					delay = ($el.attr('data-appear-animation-delay') ? $el.attr('data-appear-animation-delay') : self.options.delay);
+
+					if (delay > 1) {
+						$el.css('animation-delay', delay + 'ms');
+					}
+
+					$el.addClass($el.attr('data-appear-animation'));
+
+					setTimeout(function() {
+						$el.addClass('appear-animation-visible');
+					}, delay);
+
+				}, {
+					accX: self.options.accX,
+					accY: self.options.accY
+				});
+
+			} else {
+
+				$el.addClass('appear-animation-visible');
+
+			}
+
+			return this;
+		}
+	};
+
+	// expose to scope
+	$.extend(theme, {
+		PluginAnimate: PluginAnimate
+	});
+
+	// jquery plugin
+	$.fn.themePluginAnimate = function(opts) {
+		return this.map(function() {
+			var $this = $(this);
+
+			if ($this.data(instanceName)) {
+				return $this.data(instanceName);
+			} else {
+				return new PluginAnimate($this, opts);
+			}
+
+		});
+	};
+
+}).apply(this, [window.theme, jQuery]);
+
+// Animate
+(function($) {
+
+	'use strict';
+
+	if ($.isFunction($.fn['themePluginAnimate'])) {
+
+		$(function() {
+			$('[data-plugin-animate], [data-appear-animation]').each(function() {
+				var $this = $(this),
+					opts;
+
+				var pluginOptions = $this.data('plugin-options');
+				if (pluginOptions)
+					opts = pluginOptions;
+
+				$this.themePluginAnimate(opts);
+			});
+		});
+
+	}
+
+}).apply(this, [jQuery]);
+
+ 
+           /////////// amination action itacumens ////////////////////
+ 
+(function($) {
+    $.fn.appear = function(fn, options) {
+
+        var settings = $.extend({
+
+            //arbitrary data to pass to fn
+            data: undefined,
+
+            //call fn only on the first appear?
+            one: true,
+
+            // X & Y accuracy
+            accX: 0,
+            accY: 0
+
+        }, options);
+
+        return this.each(function() {
+
+            var t = $(this);
+
+            //whether the element is currently visible
+            t.appeared = false;
+
+            if (!fn) {
+
+                //trigger the custom event
+                t.trigger('appear', settings.data);
+                return;
+            }
+
+            var w = $(window);
+
+            //fires the appear event when appropriate
+            var check = function() {
+
+                //is the element hidden?
+                if (!t.is(':visible')) {
+
+                    //it became hidden
+                    t.appeared = false;
+                    return;
+                }
+
+                //is the element inside the visible window?
+                var a = w.scrollLeft();
+                var b = w.scrollTop();
+                var o = t.offset();
+                var x = o.left;
+                var y = o.top;
+
+                var ax = settings.accX;
+                var ay = settings.accY;
+                var th = t.height();
+                var wh = w.height();
+                var tw = t.width();
+                var ww = w.width();
+
+                if (y + th + ay >= b &&
+                    y <= b + wh + ay &&
+                    x + tw + ax >= a &&
+                    x <= a + ww + ax) {
+
+                    //trigger the custom event
+                    if (!t.appeared) t.trigger('appear', settings.data);
+
+                } else {
+
+                    //it scrolled out of view
+                    t.appeared = false;
+                }
+            };
+
+            //create a modified fn with some additional logic
+            var modifiedFn = function() {
+
+                //mark the element as visible
+                t.appeared = true;
+
+                //is this supposed to happen only once?
+                if (settings.one) {
+
+                    //remove the check
+                    w.unbind('scroll', check);
+                    var i = $.inArray(check, $.fn.appear.checks);
+                    if (i >= 0) $.fn.appear.checks.splice(i, 1);
+                }
+
+                //trigger the original fn
+                fn.apply(this, arguments);
+            };
+
+            //bind the modified fn to the element
+            if (settings.one) t.one('appear', settings.data, modifiedFn);
+            else t.bind('appear', settings.data, modifiedFn);
+
+            //check whenever the window scrolls
+            w.scroll(check);
+
+            //check whenever the dom changes
+            $.fn.appear.checks.push(check);
+
+            //check now
+            (check)();
+        });
+    };
+
+    //keep a queue of appearance checks
+    $.extend($.fn.appear, {
+
+        checks: [],
+        timeout: null,
+
+        //process the queue
+        checkAll: function() {
+            var length = $.fn.appear.checks.length;
+            if (length > 0) while (length--) ($.fn.appear.checks[length])();
+        },
+
+        //check the queue asynchronously
+        run: function() {
+            if ($.fn.appear.timeout) clearTimeout($.fn.appear.timeout);
+            $.fn.appear.timeout = setTimeout($.fn.appear.checkAll, 20);
+        }
+    });
+
+ 
+    $.each(['append', 'prepend', 'after', 'before', 'attr',
+        'removeAttr', 'addClass', 'removeClass', 'toggleClass',
+        'remove', 'css', 'show', 'hide'], function(i, n) {
+        var old = $.fn[n];
+        if (old) {
+            $.fn[n] = function() {
+                var r = old.apply(this, arguments);
+                $.fn.appear.run();
+                return r;
+            }
+        }
+    });
+
+})(jQuery);
+
